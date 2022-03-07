87fd6b07fc62c45247998635ea4dfb4952e39a52
Reza Kashkoul <rezakashkoul@gmail.com>
2022-03-07 12:44:56 +0330

    Initial Commit

---
+++ colori/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  colori
+//
+//  Created by Reza Kashkoul on 16-Esfand-1400 .
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
